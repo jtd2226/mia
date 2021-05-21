2fb6b8dd9e0bc46affef70c7489c85cae56135b3
Naysa Moreno <naysaymoreno@gmail.com>
2021-05-20 20:04:24 -0500

    wrote spotify requests to obtain user token and store in firebase
    Co-authored-by: Davis,Joel <d101985@heb.com>

--- pagecomponents/presave.js
+++ pagecomponents/presave.js
@@ -6,9 +6,26 @@ const client_id = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
 const client_secret = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET;
 const response_type = 'code';
 const redirect_uri = 'http://localhost:5000/presave';
-const spotify = Request.path('https://accounts.spotify.com');
+const spotify = {
+  auth: Request.path('https://accounts.spotify.com'),
+  api: Request.path('https://api.spotify.com/v1'),
+  setSession(session) {
+    spotify.session = session;
+    localStorage.setItem('email', session.email);
+    spotify.api = spotify.api.headers({
+      Authorization: `Bearer ${session.access_token}`,
+    });
+  },
+};
+
+if (process.browser) {
+  spotify.token = spotify.auth.POST.path('api/token').headers({
+    Authorization: `Basic ${btoa(`${client_id}:${client_secret}`)}`,
+  });
+}
+
 function login() {
-  const url = spotify.path('authorize').query({
+  const url = spotify.auth.path('authorize').query({
     client_id,
     client_secret,
     response_type,
@@ -17,26 +34,53 @@ function login() {
   }).fullURL;
   location.replace(url);
 }
+
+function refresh() {
+  const refresh_token = '???';
+  return spotify.token.form({
+    grant_type: 'refresh_token',
+    refresh_token,
+  });
+}
+
 function authorize(code) {
   const grant_type = 'authorization_code';
-  return (
-    spotify.POST.path('api/token')
-      // .headers({
-      //   Authorization: `Basic ${btoa(`${client_id}:${client_secret}`)}`,
-      // })
-      .form({ grant_type, code, redirect_uri, client_id, client_secret })
-      .send()
-      .then(saveCredentials)
-  );
+  return spotify.token
+    .form({ grant_type, code, redirect_uri })
+    .send()
+    .then(session => {
+      spotify.setSession(session);
+      return spotify.api
+        .path('me')
+        .send()
+        .then(user => ({ user, session }));
+    })
+    .then(saveCredentials);
 }
+
 function saveCredentials(credentials) {
-  fetch('/api/spotify/store', {
-    method: 'POST',
-    body: JSON.stringify(credentials),
-  })
-    .then(r => r.json())
-    .then(data => (data.error ? console.error(data.error) : console.log(data)))
-    .catch(console.error);
+  return Request.POST.path('/api/spotify/store').json(credentials).send();
+}
+
+function getMiasSong() {
+  return spotify.api
+    .path('search')
+    .query({
+      q: 'upc:195939895060',
+      type: 'track,album',
+    })
+    .then(data => {
+      console.log(data);
+    });
+}
+
+/*
+curl -X "PUT" "https://api.spotify.com/v1/me/tracks?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQCdqJ3FBy4GcNT_DS9DMygYbEU-kwwsFN8BW3vYLA9_CmGwMhHKTz_ngO5flp1tZMQUV5mPpZAEs8CoLVbii59heDvnHOCFGDYCH29oSDverGDv7Yv9X1rrARRfM-NR5-HdLpVtl-lDvXe3IbMiOprNPKu-ZRtwiGGcW9li_zxupOFwM2k"
+*/
+function likeMiasSong() {
+  return getMiasSong().then(({ id }) =>
+    spotify.api.PUT.path('me/tracks').query({ ids: id })
+  );
 }
 
 export default function PreSave() {
