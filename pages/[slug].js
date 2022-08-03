0f8ddf81c84da778b9909b31068c69ce1ab97f36
jtd2226 <joeldavi@utexas.edu>
2022-08-03 17:16:36 -0500

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic';
 import Router from '../routes/router';
 
 const Backgrounds = {
-  defaultImage: '/img/MAMA/angelmama.jpg',
+  defaultImage: '/img/MAMA/grassymomma.jpeg',
   main: {
     // component: ({ images, children }) => (
     //   <>
