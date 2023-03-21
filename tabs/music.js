e911ddfbba3e28c263c62956b490702caf73e170
Joel Davis <joel_davis2@apple.com>
2023-03-21 14:13:44 -0500

    remove old embeds

--- tabs/music.js
+++ tabs/music.js
@@ -27,13 +27,6 @@ export default function Home() {
         url="https://open.spotify.com/embed/track/31FTac3rJ5fYidpA8axd2r"
         compact
       />
-      <EmbeddedAlbum
-        url="https://open.spotify.com/embed/album/6dYcouiQ3ywpXgNfwmLdlR"
-        height="152px"
-      />
-      {/* <EmbeddedAlbum url="https://embed.music.apple.com/us/album/imminent-euphoria-ep/1535333016" /> */}
-      <EmbeddedAlbum url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/991434859&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" />
-      <EmbeddedAlbum url="https://bandcamp.com/EmbeddedPlayer/album=3920738786/size=large/bgcol=ffffff/linkcol=f171a2/artwork=small/transparent=true/" />
     </>
   );
 }
