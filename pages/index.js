917ee5a729ff0af1d7353f2a6cfcc901d52eeb16
Joel Davis <joel_davis2@apple.com>
2023-06-18 03:05:03 -0500

    try out rgb shifting

--- pages/index.js
+++ pages/index.js
@@ -92,6 +92,7 @@ export default function Home({ children }) {
         <World
           images="/text/acid_lovpune.png"
           amplitude={-1}
+          rgbshift={5}
           style={{ width: '100%', height: '100%' }}
         />
       </Link>
