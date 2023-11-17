4ffb9d0bd35de0d044a0cbfb02b24644e61590ea
Joel Davis <joel_davis2@apple.com>
2023-11-16 18:40:25 -0600

    update album art

--- pagecomponents/home.js
+++ pagecomponents/home.js
@@ -22,14 +22,14 @@ export default function Home() {
         <a>Click to Pre-Save</a>
       </Link> */}
       <a
-        href="https://open.spotify.com/album/5iqD2ZXFfZG8XLZpiEqyon"
+        href="https://open.spotify.com/album/1EpBenE7byXtAUoLWBz1Ud"
         target="_blank"
         rel="noreferrer"
         className="album-link"
       >
         <span className="album">
           <World
-            images="/img/MAMA/albumlast_time.jpg"
+            images="/img/MAMA/album_neverknew.jpeg"
             amplitude={-5}
             glitch
             style={{ width: '275px', height: '275px' }}
