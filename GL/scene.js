3ad4a36e8809d8fe32a5d66bb8e630191edccc55
Joel Davis <joel_davis2@apple.com>
2023-11-16 19:41:17 -0600

    add home page carousel

--- GL/scene.js
+++ GL/scene.js
@@ -293,7 +293,7 @@ export default function World({
     return () => {
       scene.dispose();
     };
-  }, []);
+  }, [images]);
   return (
     <>
       {fallback && (
--- pagecomponents/home.js
+++ pagecomponents/home.js
@@ -1,6 +1,18 @@
 import World from 'GL/scene';
 import Image from 'next/image';
+import { useCallback, useEffect, useState, useRef } from 'react';
 // import { Link } from '../routes/router';
+
+const images = [
+  {
+    src: '/img/MAMA/album_neverknew.jpeg',
+    href: 'https://open.spotify.com/album/1EpBenE7byXtAUoLWBz1Ud',
+  },
+  {
+    src: '/img/MAMA/album_allmyfriends.jpeg',
+    href: 'https://open.spotify.com/album/3Yx7zZ43S8fFYqeNN5tqK0',
+  },
+];
 
 const Container = ({ children }) => (
   <div
@@ -15,28 +27,97 @@ const Container = ({ children }) => (
   </div>
 );
 
+function Carousel({ images }) {
+  const [index, setIndex] = useState(0);
+  const { src, href, ...rest } = images[index];
+  const interval = useRef();
+
+  const next = useCallback(() => {
+    setIndex(old => {
+      const next = old + 1;
+      if (next >= images.length) return 0;
+      return next;
+    });
+  }, [images]);
+
+  const reset = useCallback(() => {
+    clearInterval(interval.current);
+    interval.current = setInterval(() => {
+      next();
+    }, 8000);
+  }, [next]);
+
+  const back = useCallback(() => {
+    reset();
+    setIndex(old => {
+      const next = old - 1;
+      if (next < 0) return images.length - 1;
+      return next;
+    });
+  }, [images, reset]);
+
+  useEffect(() => {
+    reset();
+
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, [next]);
+
+  return (
+    <a href={href} target="_blank" rel="noreferrer" className="album-link">
+      <span className="album">
+        <span className="album-carousel">
+          <button
+            onClick={e => {
+              e.preventDefault();
+              e.stopPropagation();
+              back();
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.729 84.721">
+              <path
+                d="M47.728 42.36C47.728 41.139 47.283 40.06 46.34 39.129L7.645 1.305C6.827.435 5.74 0 4.496 0 1.958 0 0 1.896 0 4.444 0 5.636.539 6.764 1.295 7.624L36.83 42.361 1.295 77.097C.539 77.957 0 79.034 0 80.277 0 82.826 1.958 84.721 4.496 84.721 5.739 84.721 6.826 84.286 7.646 83.416L46.34 45.594C47.283 44.66 47.728 43.584 47.728 42.36Z"
+                fill="currentcolor"
+              ></path>
+            </svg>
+          </button>
+          <World
+            images={src}
+            amplitude={-5}
+            glitch
+            style={{ width: '275px', height: '275px' }}
+            {...rest}
+          />
+          <button
+            onClick={e => {
+              e.preventDefault();
+              e.stopPropagation();
+              reset();
+              next();
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.729 84.721">
+              <path
+                d="M47.728 42.36C47.728 41.139 47.283 40.06 46.34 39.129L7.645 1.305C6.827.435 5.74 0 4.496 0 1.958 0 0 1.896 0 4.444 0 5.636.539 6.764 1.295 7.624L36.83 42.361 1.295 77.097C.539 77.957 0 79.034 0 80.277 0 82.826 1.958 84.721 4.496 84.721 5.739 84.721 6.826 84.286 7.646 83.416L46.34 45.594C47.283 44.66 47.728 43.584 47.728 42.36Z"
+                fill="currentcolor"
+              ></path>
+            </svg>
+          </button>
+        </span>
+        <Image src="/text/albumtext.png" width={350} height={67} />
+      </span>
+    </a>
+  );
+}
+
 export default function Home() {
   return (
     <Container>
       {/* <Link route="presave">
         <a>Click to Pre-Save</a>
       </Link> */}
-      <a
-        href="https://open.spotify.com/album/1EpBenE7byXtAUoLWBz1Ud"
-        target="_blank"
-        rel="noreferrer"
-        className="album-link"
-      >
-        <span className="album">
-          <World
-            images="/img/MAMA/album_neverknew.jpeg"
-            amplitude={-5}
-            glitch
-            style={{ width: '275px', height: '275px' }}
-          />
-          <Image src="/text/albumtext.png" width={350} height={67} />
-        </span>
-      </a>
+      <Carousel images={images} />
     </Container>
   );
 }
