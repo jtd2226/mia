ec63c97f2b1d2e9ad7c75d1e28466a08390fc7c2
jtd2226 <joel@joeltdavis.com>
2026-07-21 18:31:22 -0500

    test for errors

--- pages/api/rsvp.js
+++ pages/api/rsvp.js
@@ -1,57 +1,59 @@
 import { firestore } from 'firebase';
 
 export default async function handler(request, response) {
-  const {
-    body: { name, email, phone },
-  } = request;
+  try {
+    const {
+      body: { name, email, phone },
+    } = request;
 
-  const ID = email || phone;
-  if (!ID) {
-    return response.status(400).json({ error: 'Email or Phone is required' });
-  }
+    const ID = email || phone;
+    if (!ID) {
+      return response.status(400).json({ error: 'Email or Phone is required' });
+    }
 
-  const user = await firestore
-    .collection('users')
-    .doc(ID)
-    .get()
-    .then(doc => {
-      if (doc.exists) {
-        return doc.data();
-      } else {
-        return {
-          ID,
-          Created: new Date().getTime(),
-          Presaved: false,
-        };
-      }
+    const user = await firestore
+      .collection('users')
+      .doc(ID)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return doc.data();
+        } else {
+          return {
+            ID,
+            Created: new Date().getTime(),
+            Presaved: false,
+          };
+        }
+      });
+
+    user.Email = email || user.Email || '';
+    user.Phone = phone || user.Phone || '';
+    user.Name = name || user.Name || '';
+
+    user.Joined ??= new Date(user.Created).toLocaleDateString();
+    user.AcquisitionChannel = 'https://lovpune.com';
+
+    await fetch(`http://ip-api.com/json`)
+      .then(res => res.json())
+      .then(location => {
+        user.City = location.city;
+        user.State = location.region;
+        user.Country = location.country;
+        user.IP = location.query;
+        user.location = location;
+      });
+
+    Object.keys(user).forEach(key => {
+      user[key] ??= '';
     });
 
-  user.Email = email || user.Email || '';
-  user.Phone = phone || user.Phone || '';
-  user.Name = name || user.Name || '';
-
-  user.Joined ??= new Date(user.Created).toLocaleDateString();
-  user.AcquisitionChannel = 'https://lovpune.com';
-
-  await fetch(`http://ip-api.com/json`)
-    .then(res => res.json())
-    .then(location => {
-      user.City = location.city;
-      user.State = location.region;
-      user.Country = location.country;
-      user.IP = location.query;
-      user.location = location;
-    })
-    .catch(error => console.log(error));
-
-  Object.keys(user).forEach(key => {
-    user[key] ??= '';
-  });
-
-  return firestore
-    .collection('users')
-    .doc(ID)
-    .set(user)
-    .then(() => response.status(200).json({ success: true }))
-    .catch(error => response.status(500).json({ error }));
+    return firestore
+      .collection('users')
+      .doc(ID)
+      .set(user)
+      .then(() => response.status(200).json({ success: true }));
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
 }
