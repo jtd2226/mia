8ff077f77c94722a90a098a1c8c80115b24ff7d0
jtd2226 <joel@joeltdavis.com>
2026-07-21 19:02:49 -0500

    call dashboard api instead

--- pages/api/rsvp.js
+++ pages/api/rsvp.js
@@ -1,60 +1,15 @@
-import { firestore } from 'firebase';
-
 export default async function handler(request, response) {
   try {
-    response.setHeader('response-type', 'application/json');
-    const {
-      body: { name, email, phone },
-    } = request;
-
-    const ID = email || phone;
-    if (!ID) {
-      return response.status(400).json({ error: 'Email or Phone is required' });
-    }
-
-    const user = await firestore
-      .collection('users')
-      .doc(ID)
-      .get()
-      .then(doc => {
-        if (doc.exists) {
-          return doc.data();
-        } else {
-          return {
-            ID,
-            Created: new Date().getTime(),
-            Presaved: false,
-          };
-        }
-      });
-
-    user.Email = email || user.Email || '';
-    user.Phone = phone || user.Phone || '';
-    user.Name = name || user.Name || '';
-
-    user.Joined ??= new Date(user.Created).toLocaleDateString();
-    user.AcquisitionChannel = 'https://lovpune.com';
-
-    await fetch(`http://ip-api.com/json`)
+    return fetch('https://dash.lovpune.com/api/rsvp', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(request.body),
+    })
       .then(res => res.json())
-      .then(location => {
-        user.City = location.city;
-        user.State = location.region;
-        user.Country = location.country;
-        user.IP = location.query;
-        user.location = location;
-      });
-
-    Object.keys(user).forEach(key => {
-      user[key] ??= '';
-    });
-
-    return firestore
-      .collection('users')
-      .doc(ID)
-      .set(user)
-      .then(() => response.status(200).json({ success: true }));
+      .then(data => response.status(200).json(data));
   } catch (error) {
-    return response.status(200).json({ error });
+    return response.status(500).json({ error });
   }
 }
